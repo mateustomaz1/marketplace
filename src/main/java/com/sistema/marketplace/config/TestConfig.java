72d231dbab3395840399122e173e610876795097
Klebson Amarante <97056064+KlebsonAmaranteS@users.noreply.github.com>
2025-02-18 15:29:32 -0300

    Update TestConfig.java

--- src/main/java/com/sistema/marketplace/config/TestConfig.java
+++ src/main/java/com/sistema/marketplace/config/TestConfig.java
@@ -22,7 +22,7 @@ import com.sistema.marketplace.repositories.ProductRepository;
 import com.sistema.marketplace.repositories.UserRepository;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig implements CommandLineRunner {
 
 	@Autowired
