c4fff56558ff106481c95d0868a2f295cbccbcd3
Klebson Amarante <97056064+KlebsonAmaranteS@users.noreply.github.com>
2025-02-18 16:40:09 -0300

    Delete marketplace/marketplace-main directory para correção

--- marketplace/marketplace-main/src/main/java/com/sistema/marketplace/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.marketplace.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sistema.marketplace.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
